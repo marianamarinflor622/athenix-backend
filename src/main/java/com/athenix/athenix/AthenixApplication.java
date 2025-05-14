fe51c749f666e716055ad8aff3b9372eff3ec488
marianamarinflor622 <marianamarinflor@gmail.com>
2025-05-14 14:55:24 +0200

    first commit

---
+++ src/main/java/com/athenix/athenix/AthenixApplication.java
@@ -0,0 +1,13 @@
+package com.athenix.athenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthenixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthenixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/athenix/athenix/AthenixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athenix.athenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
